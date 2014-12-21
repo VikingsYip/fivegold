169257ccacc725590f07c8505bfc583ec0e57b5e
VikingsYip <chinakings@gmail.com>
2014-12-21 22:56:58 +0800

    add shopcart

---
+++ fivegold.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+function request(paras){ 
+var url = location.href;  
+var paraString = url.substring(url.indexOf("?")+1,url.length).split("&");  
+var paraObj = {}  
+for (i=0; j=paraString[i]; i++){  
+paraObj[j.substring(0,j.indexOf("=")).toLowerCase()] = j.substring(j.indexOf("=")+1,j.length);  
+}  
+var returnValue = paraObj[paras.toLowerCase()];  
+if(typeof(returnValue)=="undefined"){  
+return "";  
+}else{  
+return returnValue;  
+}  
+}
+
+$("#5gshoplist").bind("swipeleft", function() {  
+   history.go(-1)
+});  
+
+var myScroll;
+function loaded() {
+	myScroll = new iScroll('wrapper');
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+/* * * * * * * *
+ *
+ * Use this for high compatibility (iDevice + Android)
+ *
+ */
+document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+/*
+ * * * * * * * */
+
